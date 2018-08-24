3fcf61d7d18c412d0d4996cdae6a6af4a3d947cd
Aakash Sarang <sarangaakash@gmail.com>
2018-08-24 15:49:09 -0700

    Make post route for new user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post('/register', (req, res) => {
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if (user) {
+                return res.status(400).json({email: "Email Already in use"})
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+            }
+        })
+})
 module.exports = router;
