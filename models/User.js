8fb92830aa99177fc54e0c47c301ccc7f007d765
Aakash Sarang <sarangaakash@gmail.com>
2018-08-24 21:01:12 -0700

    Write and test login and signup functionality

--- models/User.js
+++ models/User.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const jsonwebtoken = require('jsonwebtoken');
 const UserSchema = new Schema({
     name: {
         type: String,
         required: [true, 'Name is required'],
-        // validate: {
-        //     validator: (name) => name.length > 1,
-        //     message: "Name must be atleast two characters long"
-        // }
+        validate: {
+            validator: (name) => name.length > 1,
+            message: "Name must be atleast two characters long"
+        }
     },
     email: {
         type: String,
-        // required: [true, "email is required"]
+        required: [true, "email is required"]
     },
     password: {
         type: String,
         required: [true, "Password is required"],
-        // validate: {
-        //     validator: (pass) => pass.length > 5,
-        //     message: "Password must be atleast 6 characters long"
-        // }
+        validate: {
+            validator: (pass) => pass.length > 5,
+            message: "Password must be atleast 6 characters long"
+        }
     },
     date: {
         type: Date,
--- routes/api/users.js
+++ routes/api/users.js
@@ -17,16 +17,39 @@ router.post('/register', (req, res) => {
                     password: req.body.password
                 })
                 bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(newUser.password, salt, (hash, err) => {
-                        // console.log(err + '--------------');
-                        if (err) throw err;
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        // console.log('--------------');
+                        if (err) console.log(err);
+                        // console.log('--------------');
                         newUser.password = hash;
+                        // console.log('new USer' + newUser);
+                        // console.log('hash' + hash)
                         newUser.save()
                             .then(user => res.json(user))
                             .catch(err => console.log(err));
                     })
                 })
             }
+        })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(req.body)
+    User.findOne({email})
+        .then( user => {
+            if(!user) {
+                return res.status(404).json({ email: 'This user does not exist'} );
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: 'Success' });
+                    } else {
+                        return res.status(400).json({password: 'Incorrect password'});
+                    }
+                })
         })
 })
 module.exports = router;
