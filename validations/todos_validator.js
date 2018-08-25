952a9aff4e02222d5d02c8e4298a996fcaea7585
Aakash Sarang <sarangaakash@gmail.com>
2018-08-24 22:13:39 -0700

    Write validations for todos

---
+++ validations/todos_validator.js
@@ -0,0 +1,20 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = validateTodoInput(data) {
+    let errors = {};
+    data.title = !isEmpty(data.title) ? data.title : '';
+    data.description = !isEmpty(data.description) ? data.description : '';
+    if (!Validator.isLength(data.title, {min: 4, max: 300})) {
+        errors.title = 'Todo title must be between 4-300 characters long';
+    }
+
+    if(!Validator.isEmpty(data.title)) {
+        errors.title = 'Title field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
